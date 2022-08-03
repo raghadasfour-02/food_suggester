6ac9cbcc4a0438bcd1c732f76b65d385b206ba1b
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-03 17:26:37 -0400

    Implement Food Suggesting API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './App.css'
+
+const App = () =>{
+    const ShowOptions = () =>{
+      fetch ('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then(res=>res.json())
+      .then(data=> showFoodSuggestion(data.meals))
+    }
+
+    const showFoodSuggestion = foods => {
+      foods.map((food) => {
+        alert(food.strArea)
+      })
+    }
+
+    return <button onClick={ShowOptions}> Choose my food</button>
 }
 
 export default App;
