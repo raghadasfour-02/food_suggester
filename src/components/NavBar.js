ed2a00b5815ea6572f03b2a73eec7207848633e2
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-12 00:25:24 -0400

    Create and Position Nav Bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Toolbar, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles"; 
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#fff",
+        ['@media (max-width:780px)']: { 
+           flexDirection: "column"
+          }
+    },
+    menuItem: {
+        cursor: "pointer", 
+        flexGrow: 1,
+        "&:hover": {
+            color:  "#4f25c8"
+        },
+        ['@media (max-width:780px)']: { 
+            paddingBottom: "1rem"    }
+    }
+})
+
+function NavBar() {
+    const classes = styles()
+    return (
+            <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
+                <Typography variant="h6" className={classes.menuItem}>
+                   About this project
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Other Projects
+                </Typography>
+            </Toolbar>
+    )
+}
+
+export default NavBar
