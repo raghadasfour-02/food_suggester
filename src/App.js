6e386ad809b959d1b48898df3d7addbd1cfe22ac
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-08-12 00:24:35 -0400

    Create Food Suggestions Button
    Create and center Food button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import {Typography} from '@material-ui/core'; 
+import NavBar from './components/NavBar'
+import FoodBtn from './components/FoodBtn'
 import './App.css';
 
-import React from 'react';
-import './App.css'
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
 
-const App = () =>{
-    const ShowOptions = () =>{
-      fetch ('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then(res=>res.json())
-      .then(data=> showFoodSuggestion(data.meals))
-    }
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center"
+  },
+  bigSpace: {
+    marginTop: "10rem"
+  },
+  littleSpace:{
+    marginTop: "2.5rem",
+  },
+  grid:{
+    display: "flex", 
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap", 
+  },
+})
 
-    const showFoodSuggestion = foods => {
-      foods.map((food) => {
-        alert(food.strArea)
-      })
-    }
+function App() {
+  const classes = styles(); 
 
-    return <button onClick={ShowOptions}> Choose my food</button>
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <NavBar/>
+        <div className={classes.wrapper}>
+          <Typography variant="h4" className={classes.bigSpace} color="primary">
+             Ready for some {'🔥'} food suggestions?
+          </Typography>
+          <div className={`${classes.grid} ${classes.littleSpace}`}>
+            <FoodBtn txt="Gimme food suggestion!"/>
+          </div>
+        </div>
+        <div className={classes.bigSpace}>
+        </div>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/FoodBtn.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Button} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles'
+
+const StyledButton = withStyles({
+    root: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "44px",
+        padding: "0 25px",
+        boxSizing: "border-box",
+        borderRadius: 0, 
+        background: "#4f25f7",
+        color: "#fff",
+        transform: "none",
+        boxShadow: "6px 6px 0 0 #c7d8ed",
+        transition: "background .3s,border-color .3s,color .3s",
+        "&:hover": {
+            backgroundColor:  "#4f25f7"
+          },
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  })(Button);
+
+
+const FoodBtn = () =>{
+    const ShowOptions = () =>{
+      fetch ('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then(res=>res.json())
+      .then(data=> showFoodSuggestion(data.meals))
+    }
+
+    const showFoodSuggestion = foods => {
+      foods.map((food) => {
+        alert(food.strArea)
+      })
+    }
+
+    return <StyledButton onClick={ShowOptions} variant="contained"> Gimme food suggestion! </StyledButton>
+}
+
+export default FoodBtn
